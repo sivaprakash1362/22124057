3a06c37de036412101656741523babcd81b51b67
Sivaprakash J <82955006+sivaprakash1362@users.noreply.github.com>
2024-10-09 15:01:23 +0530

    Add files via upload

---
+++ string_length.java
@@ -0,0 +1,8 @@
+public class CharacterCounter {
+    public static void main(String[] args) {
+        String input = "Hello Jenkins, Docker, and GitHub!";
+        System.out.println("The input string is: " + input);
+        int characterCount = input.length();
+        System.out.println("The number of characters in the string: " + characterCount);
+    }
+}
